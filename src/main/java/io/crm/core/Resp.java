f2dad6207ad4c9e49da90b788ca921bed61fa803
codefacts <codefacts63@gmail.com>
2015-09-20 15:32:30 +0600

    Json-Schema-validation

--- src/main/java/io/crm/core/Resp.java
+++ src/main/java/io/crm/core/Resp.java
@@ -1,8 +1,16 @@
 package io.crm.core;
 
+import io.crm.util.Util;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+
+import java.util.Collections;
+
 /**
  * Created by someone on 08/09/2015.
  */
 final public class Resp {
-
+    public static void main(String... args) {
+        Util.EMPTY_JSON_OBJECT.put("Sona", "Dola");
+    }
 }
